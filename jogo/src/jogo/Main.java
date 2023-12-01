dcb4200204c3dcbb9dd74f2ab4893f98114ae57e
João Martins <90796699+JoaoEduardoFM@users.noreply.github.com>
2023-12-01 01:44:03 -0300

    criado logica menu

--- jogo/src/jogo/Main.java
+++ jogo/src/jogo/Main.java
@@ -8,31 +8,74 @@ import jplay.Window;
 
 public class Main {
 
-	public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
-		// resolucao do jogo
-		Window janela = new Window(800, 600);
+        // resolucao do jogo
+        Window janela = new Window(960, 720);
 
-		/*
-		 * GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		 * GraphicsDevice gd = ge.getDefaultScreenDevice(); DisplayMode mode =
-		 * gd.getDisplayMode();
-		 * 
-		 * Window janela = new Window(mode.getWidth(), mode.getHeight());
-		 */
+        // imagem do menu
+        Keyboard teclado = janela.getKeyboard();
 
-		// imagem do menu
-		GameImage plano = new GameImage(URL.sprite("menu.png"));
-		Keyboard teclado = janela.getKeyboard();
+        GameImage menuStart = menu("start2.gif");
+        GameImage menuOptions = menu("options2.gif");
+        GameImage menuExit = menu("exit2.gif");
 
-		while (true) {
-			plano.draw();// printa a tela
-			janela.update();// atualiza em quanto percorre o loop
+        GameImage currentMenu = menuStart;
 
-			if (teclado.keyDown(Keyboard.ENTER_KEY)) {
-				new Pantano(janela);
-			}
-		}
-	}
+        while (true) {
+            currentMenu.draw();// printa a tela
+            janela.update();// atualiza em quanto percorre o loop
+
+            if (teclado.keyDown(Keyboard.DOWN_KEY)) {
+                // Determina o próximo menu com base no menu atual
+                if (currentMenu == menuStart) {
+                    currentMenu = menuOptions;                  
+                } else if (currentMenu == menuOptions) {
+                    currentMenu = menuExit;
+                } else if (currentMenu == menuExit) {
+                    // Volta para o menu inicial se já estiver no último menu
+                    currentMenu = menuStart;
+                }
+
+                // Aguarda a tecla ser solta para evitar trocas rápidas de menu
+                while (teclado.keyDown(Keyboard.DOWN_KEY)) {
+                    Thread.sleep(10);
+                }
+            }else if (teclado.keyDown(Keyboard.UP_KEY)) {
+                // Determina o próximo menu com base no menu atual
+                if (currentMenu == menuStart) {
+                    currentMenu = menuExit;
+                } else if (currentMenu == menuOptions) {
+                    currentMenu = menuStart;
+                } else if (currentMenu == menuExit) {
+                    // Volta para o menu inicial se já estiver no último menu
+                    currentMenu = menuOptions;
+                }
+
+                // Aguarda a tecla ser solta para evitar trocas rápidas de menu
+                while (teclado.keyDown(Keyboard.UP_KEY)) {
+                    Thread.sleep(10);
+                }
+            }
+            
+            if(teclado.keyDown(Keyboard.ENTER_KEY) && currentMenu == menuStart) {
+            	new Pantano(janela);
+            }
+            
+            if(teclado.keyDown(Keyboard.ENTER_KEY) && currentMenu == menuExit) {
+            	System.exit(0);
+            }
+            
+            
+        }
+    }
+
+    public static GameImage menu(String menuImage) {
+        return new GameImage(URL.sprite(menuImage));
+    }
+    
+    public static void shutDown(Window janela) {
+        janela.exit();  // Fecha a janela
+    }
 
 }
